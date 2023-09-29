b62f91871d49ae255251110c00135b8fdd7a5593
AnoMorCH <anomorch@gmail.com>
2023-09-29 13:01:04 +0300

    add back-end urls

---
+++ browser/src/js/consts/back-end-urls.js
@@ -0,0 +1,8 @@
+const SERVLETS_URL = "http://localhost:8080/servlets/";
+
+export const BACK_END_URLS = {
+  "log-in-servlet": SERVLETS_URL + "log-in",
+  "log-out-servlet": SERVLETS_URL + "log-out",
+  "sign-up-servlet": SERVLETS_URL + "sign-up",
+  "is-authorized-servlet": SERVLETS_URL + "is-authorized",
+};
