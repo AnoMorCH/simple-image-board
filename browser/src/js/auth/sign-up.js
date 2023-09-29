f97e0cb1caa9ee154add17ffe8f1f7fbfb8c66f5
AnoMorCH <anomorch@gmail.com>
2023-09-29 13:01:37 +0300

    implement sign-up.js

---
+++ browser/src/js/auth/sign-up.js
@@ -0,0 +1,21 @@
+import { Auth } from "../classes/auth";
+import { Msg } from "../classes/msg";
+
+const signUpForm = document.getElementById("sign-up-form");
+
+signUpForm.addEventListener("submit", e => {
+  e.preventDefault(); // prevent page reloading on submit of the form
+
+  const nickname = document.getElementById("nickname").value;
+  const password = document.getElementById("password").value;
+
+  if (Auth.isDataValid(nickname, password)) {
+    const promisedResponse = Auth.signUp(nickname, password);
+    promisedResponse.then(rawResponse => {
+      const response = JSON.parse(rawResponse);
+      new Msg(response["success"], response["comment"]).show();
+    });
+  } else {
+    new Msg(false).show(false);
+  }
+});
