4ffa70f0ba4d2fb7b85b8387e1b3973869cc3497
AnoMorCH <anomorch@gmail.com>
2023-10-01 13:28:03 +0300

    improve the back-end validation

--- back-end/servlets/src/main/java/com/classes/Validator.java
+++ back-end/servlets/src/main/java/com/classes/Validator.java
@@ -1,10 +1,12 @@
 package com.classes;
 
+import java.util.regex.Pattern;
+
 /**
- * Please, note that the validation is quite simple: it only checks on 
- * front-end and back-end if a password is not less than 8 arbitrary 
- * characters and a nickname is not less than 1 arbitrary character. The 
- * reason is simple: this is a study project and there is no need in complex 
+ * Please, note that the validation is quite simple: it only checks on
+ * front-end and back-end if a password is not less than 8 arbitrary
+ * characters and a nickname is not less than 1 arbitrary character. The
+ * reason is simple: this is a study project and there is no need in complex
  * validation.
  */
 
@@ -14,6 +16,7 @@ public class Validator {
 
     /**
      * Check if validation data is correct.
+     * 
      * @param nickname A user's name.
      * @param password A user's password.
      * @return If a user's data is correct.
@@ -21,22 +24,35 @@ public class Validator {
     public static boolean isDataOk(String nickname, String password) {
         return isNicknameOk(nickname) && isPasswordOk(password);
     }
-    
+
     /**
      * Check if a nickname is valid.
+     * 
      * @param value A user's name.
      * @return If a nickname is valid.
      */
     private static boolean isNicknameOk(String value) {
         return value.length() > MIN_NICKNAME_LENGTH;
     }
-    
+
     /**
      * Check if a password is valid.
+     * 
      * @param value A user's password.
      * @return If a password is valid.
      */
     private static boolean isPasswordOk(String value) {
-        return value.length() > MIN_PASSWORD_LENGTH;
+        return value.length() > MIN_PASSWORD_LENGTH
+                && hasOnlyEngLettersAndNumbers(value);
+    }
+
+    /**
+     * Check if a string value has only English letters and numbers.
+     * 
+     * @param value A string value for check.
+     * @return If a string value has only English letters and numbers.
+     */
+    private static boolean hasOnlyEngLettersAndNumbers(String value) {
+        return Pattern.matches("^[a-zA-Z0-9]*$", value);
     }
 }
